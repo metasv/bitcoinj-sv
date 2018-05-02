e27d9570617517b1ee614582a0db324e45341e27
hashengineering <hashengineeringsolutions@gmail.com>
2018-05-01 19:33:57 -0700

    AddressFactoryTest:  Add tests for wrong network.

--- core/src/test/java/org/bitcoinj/core/AddressFactoryTest.java
+++ core/src/test/java/org/bitcoinj/core/AddressFactoryTest.java
@@ -1,6 +1,22 @@
+/*
+ * Copyright 2018 bitcoinj-cash developers
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.bitcoinj.core;
 
 import org.bitcoinj.params.MainNetParams;
+import org.bitcoinj.params.TestNet3Params;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -62,4 +78,25 @@ public class AddressFactoryTest {
             //message may contain:  "Illegal character"
         }
     }
+
+    @Test
+    public void testWrongNetwork() {
+        NetworkParameters params = TestNet3Params.get();
+        String cashAddress = "bitcoincash:qpk4hk3wuxe2uqtqc97n8atzrrr6r5mleczf9sur4h";
+        String legacyAddress = "1AyEgvE2XNM65EkdisywrZZghHuMv1ngf8";
+        try {
+            cashAddressFactory.getFromFormattedAddress(params, cashAddress);
+            fail("getFromFormattedAddress should fail with mainnet address");
+        } catch (AddressFormatException ex) {
+            //message may contain:  "Prefix of address did not match acceptable prefix for network:  bitcoincash not bchtest"
+        }
+
+        try {
+            Address.fromBase58(params, legacyAddress);
+            fail("fromBase58 should fail with a mainnet address");
+        } catch (AddressFormatException ex) {
+            //message may contain:  "version code of address did not match acceptable versions for network: 0 not in [111, 196]"
+        }
+    }
+
 }
