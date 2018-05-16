f52e94d6ae53ebde17782ffcf12004c4829a62a2
Daniel Connolly <daniel@dconnolly.com>
2018-05-16 19:47:18 +0200

    update copyright information

--- core/src/test/java/org/bitcoinj/script/ScriptDataDrivenTest.java
+++ core/src/test/java/org/bitcoinj/script/ScriptDataDrivenTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2018 BitcoinJ-Cash Developers
+ * Copyright 2018 the bitcoinj-cash developers
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
